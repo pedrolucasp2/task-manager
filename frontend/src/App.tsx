dfbf1c603c53cd9e9eb48f0bb747188427b86bfc
Pedro Pereira <pedrolucasp2@gmail.com>
2025-10-20 22:18:36 -0300

    feat(frontend): setup react, vite, tailwind, and dependencies

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="flex h-screen w-full items-center justify-center bg-gray-900">
+      <h1 className="text-3xl font-bold text-blue-500">
+        Tailwind v4 Funciona!
+      </h1>
+    </div>
+  )
+}
+export default App
